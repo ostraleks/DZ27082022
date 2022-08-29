886d537c7a46735c578ec95eb9f3491e3c6543a4
aleksndr <ostraleks@gmail.com>
2022-08-29 22:14:30 +0500

    1.1

---
+++ 1/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+
+0,5 7 -2 -0,2
+
+1 -3,3 8 -9,9
+
+8 7,8 -7,1 9
+*/
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+double[,] GetArray(int m, int n)
+{
+    double[,] matrix = new double[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round((new Random().NextDouble() * 100), 2); 
+        }
+    }
+    return matrix;
+}
+void PrintArray(double[,] inputMatrix)
+{
+    for (int i = 0; i < inputMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputMatrix.GetLength(1); j++)
+        {
+            Console.Write(inputMatrix[i, m] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+double[,] resultMatrix = GetArray(rows, columns); 
+PrintArray(resultMatrix);
---
+++ 2/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задача 50. Напишите программу, которая на вход принимает позиции элемента
+ в двумерном массиве, и возвращает значение этого элемента или же указание,
+ что такого элемента нет.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+17 -> такого числа в массиве нет
+
+*/
+
+int[,] array =
+{
+    {5, 8, 3, 1, 9, 0},
+    {7, 4, 6, 5, 1, 1},
+    {9, 0, 6, 5, 3, 3}
+};
+Console.Write("Введите номер строки: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер столбца: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+if (rows <= array.GetLength(0) && columns <= array.GetLength(1))
+{
+    Console.WriteLine($"Значение элемента {rows} строки и {columns} столбца - {array[rows, columns]}");
+}
+else Console.WriteLine("Элемент за пределами массива");
